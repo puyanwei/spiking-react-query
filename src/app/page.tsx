ecef9cbe0eaefd46956793c66719f7724eb6a733
Puyan Wei <puyanwei@gmail.com>
2024-02-10 16:54:55 +0000

    game over logic and prevent cells clicking again

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,18 +9,12 @@ type State = {
 
 const players = ["John", "Peter"]
 const state = Array(9).fill({ player: null, state: null })
-const playerSymbolMap: Record<number, string> = {
-  0: "X",
-  1: "O",
-}
 
 export default function Home() {
   const [player, setPlayer] = useState(players[0])
   const [boardState, setBoardState] = useState<State[]>(state)
 
-  const buttonStyles = "outline outline-1 outline-black h-40"
-
-  const handleOnClick = (e: MouseEvent<HTMLButtonElement>, index: number) => {
+  function handleOnClick(e: MouseEvent<HTMLButtonElement>, index: number) {
     const isFirstPlayer = player === players[0]
     isFirstPlayer ? setPlayer(players[1]) : setPlayer(players[0])
     const newBoard: State[] = boardState.map((cell, i) => {
@@ -30,13 +24,31 @@ export default function Home() {
     setBoardState(newBoard)
   }
 
+  function isGameOver() {
+    return boardState.every((cell) => cell.state !== null)
+  }
+
+  function isFilled(index: number) {
+    return boardState[index].state !== null
+  }
+
+  const buttonStyles = "outline outline-1 outline-black h-40"
+
   return (
     <main className="min-h-screen w-[30rem] mx-auto">
       <h1 className="text-6xl text-center p-12">Tic Tac Toe</h1>
-      <h2 className="py-4 text-xl">{`${player}'s turn`}</h2>
+      <h2 className="py-4 text-xl">
+        {`${player}'s turn`} {isGameOver() && <span> - Game Over</span>}
+      </h2>
+
       <div className="grid grid-cols-3 border-black border-2 text-8xl">
         {boardState.map(({ state }, index) => (
-          <button className={buttonStyles} key={index} onClick={(e) => handleOnClick(e, index)}>
+          <button
+            className={buttonStyles}
+            key={index}
+            onClick={(e) => handleOnClick(e, index)}
+            disabled={isFilled(index)}
+          >
             {state}
           </button>
         ))}
