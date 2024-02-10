b38b48c993fa769f7281d2380dcf83e116a70d67
Puyan Wei <puyanwei@gmail.com>
2024-02-10 11:57:38 +0000

    styling the tic tac toe box

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,16 @@
 export default function Home() {
-  return <main className="flex min-h-screen p-24">HELLO</main>
+  const buttonStyles = "outline outline-1 outline-black h-40"
+  const array = Array(9).fill("")
+  return (
+    <main className="min-h-screen w-[30rem] mx-auto">
+      <h1 className="text-6xl text-center p-12">Tic Tac Toe</h1>
+      <div className="grid grid-cols-3 border-black border-2 text-8xl">
+        {array.map((e, i) => (
+          <button className={buttonStyles} key={i}>
+            X
+          </button>
+        ))}
+      </div>
+    </main>
+  )
 }
